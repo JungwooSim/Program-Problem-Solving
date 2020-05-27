8520a5de80d53c52872fdfa610e3b1cd51ec6447
JungwooSim <cscd053@gmail.com>
2020-05-27 15:04:10 +0900

    FrogRiverOne ADD

--- src/me/study/Main.java
+++ src/me/study/Main.java
@@ -4,8 +4,8 @@ import me.study.codility.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] data = {3, 1, 2, 4, 3};
-        BinaryGap binaryGap = new BinaryGap();
-        System.out.println(binaryGap.solution(1162));
+        int[] data = {1, 3, 1, 4, 2, 3, 5, 4};
+        FrogRiverOne frogRiverOne = new FrogRiverOne();
+        System.out.println(frogRiverOne.solution(5, data));
     }
 }
