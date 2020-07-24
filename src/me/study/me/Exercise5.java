57fc1fbd6895c5d3f128ef22a3f7651eb1da401f
JungwooSim <cscd053@gmail.com>
2020-07-24 23:59:40 +0900

    알고리즘 공부

---
+++ src/me/study/me/Exercise5.java
@@ -0,0 +1,35 @@
+package me.study.me;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Exercise5 exercise5 = new Exercise5();
+
+//        int[] A = {3,2,-2,5,-3};
+//        int[] A = {1,1,2,-1,2,-1};
+        int[] A = {1,2,3,-4};
+
+        System.out.println(exercise5.solution(A));
+    }
+
+    public int solution(int[] A) {
+        Set temp = new HashSet();
+        int maxValue = 0;
+        int a;
+
+        for (int i=0; i<A.length; i++) {
+
+            if (temp.contains(A[i]*-1)) {
+                if (maxValue < Math.abs(A[i])) {
+                    maxValue = Math.abs(A[i]);
+                }
+            } else {
+                temp.add(A[i]);
+            }
+        }
+
+        return maxValue;
+    }
+}
