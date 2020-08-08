1add4eda3e75ec9de788777a031e93ff57db47d8
JungwooSim <cscd053@gmail.com>
2020-08-08 17:42:18 +0900

    AddTwoNumbers 시간복잡도 : O(Max(M, N)

---
+++ src/me/study/leetcode/AddTwoNumbers.java
@@ -0,0 +1,56 @@
+package me.study.leetcode;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        ListNode result = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        while (result != null) {
+            System.out.println(result.val);
+            if (result != null) result = result.next;
+        }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode p = l1;
+        ListNode q = l2;
+        int carry = 0;
+        ListNode result = new ListNode(0);
+        ListNode curr = result;
+
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return result.next;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {
+
+    }
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val; this.next = next;
+    }
+}
