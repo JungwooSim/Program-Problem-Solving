92a18addbbabd97b127de7fffde20b12fde0ae8b
JungwooSim <cscd053@gmail.com>
2020-09-02 23:53:33 +0900

    Leetcode - StringToInteger

---
+++ src/me/study/leetcode/StringToInteger.java
@@ -0,0 +1,38 @@
+package me.study.leetcode;
+
+import java.util.regex.Pattern;
+
+public class StringToInteger {
+    public static void main(String[] args) {
+        StringToInteger stringToInteger = new StringToInteger();
+
+        System.out.println(stringToInteger.myAtoi("-91283472332"));
+    }
+
+    public int myAtoi(String str) {
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+        if (str.length() == 0) return 0;
+
+        //Discard whitespaces in the beginning
+        while (i < str.length() && str.charAt(i) == ' ')
+            i++;
+
+        // Check if optional sign if it exists
+        if (i < str.length() && (str.charAt(i) == '+' || str.charAt(i) == '-'))
+            sign = (str.charAt(i++) == '-') ? -1 : 1;
+
+
+
+        // Build the result and check for overflow/underflow condition
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            if (result > Integer.MAX_VALUE / 10 ||
+                    (result == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + (str.charAt(i++) - '0');
+        }
+        return result * sign;
+    }
+}
