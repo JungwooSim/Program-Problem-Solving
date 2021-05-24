29be57f1146c9da2e42848cb9cf0b7401fc876d5
JungwooSim <cscd053@gmail.com>
2021-05-24 09:29:07 +0900

    programmers - 완주하지 못한 선수

---
+++ src/me/study/programmers/hash/lessons1.java
@@ -0,0 +1,70 @@
+package me.study.programmers.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+문제 설명
+수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
+마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+- 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.
+- completion의 길이는 participant의 길이보다 1 작습니다.
+- 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
+- 참가자 중에는 동명이인이 있을 수 있습니다.
+
+ */
+public class lessons1 {
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+
+        System.out.println(solution(participant, completion));
+        System.out.println(solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
+        System.out.println(solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
+    }
+
+    // 내가 짠거
+    public static String solution(String[] participant, String[] completion) {
+        Map<String, Integer> completionMap = new HashMap<>();
+
+        for (String value : completion) {
+
+
+            if (completionMap.containsKey(value)) {
+                completionMap.put(value, completionMap.get(value) + 1);
+            } else {
+                completionMap.put(value, 1);
+            }
+        }
+
+        for (String value : participant) {
+            if (!completionMap.containsKey(value)) {
+                return value;
+            }
+
+            completionMap.put(value, completionMap.get(value) - 1);
+
+            if (completionMap.get(value) < 0) {
+                return value;
+            }
+        }
+        return "";
+    }
+
+    // 다른사람 코드
+    public static String solution2(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
+
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
+        return answer;
+    }
+}
