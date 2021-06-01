90c4950cd661e09bcdba55849eb4b0b6e72bd3fa
JungwooSim <cscd053@gmail.com>
2021-06-01 09:16:03 +0900

    programmers - H-Index

--- src/me/study/programmers/sort/Lesson3.java
+++ src/me/study/programmers/sort/Lesson3.java
@@ -18,16 +18,15 @@ H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 
  */
 public class Lesson3 {
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{3, 0, 6, 1, 5}));
-        System.out.println(solution(new int[]{7}));
-        System.out.println(solution(new int[]{0}));
-        System.out.println(solution(new int[]{1545, 2, 999, 790, 540, 10, 22}));
-        System.out.println(solution(new int[]{1, 7, 0, 1, 6, 4}));
-        System.out.println(solution(new int[]{10, 50, 100}));
-        System.out.println(solution(new int[]{4, 3, 3, 3, 3}));
-        System.out.println(solution(new int[]{2, 7, 5}));
-        System.out.println(solution(new int[]{22, 47}));
-
+        System.out.println(solution(new int[]{3, 0, 6, 1, 5})); // return : 3
+        System.out.println(solution(new int[]{7})); // return : 1
+        System.out.println(solution(new int[]{0})); // return : 0
+        System.out.println(solution(new int[]{1545, 2, 999, 790, 540, 10, 22})); // return : 6
+        System.out.println(solution(new int[]{1, 7, 0, 1, 6, 4})); // return : 3
+        System.out.println(solution(new int[]{10, 50, 100})); // return : 3
+        System.out.println(solution(new int[]{4, 3, 3, 3, 3})); // return : 3
+        System.out.println(solution(new int[]{2, 7, 5})); // return : 2
+        System.out.println(solution(new int[]{22, 47})); // return : 2
     }
 
     public static int solution(int[] citations) {
